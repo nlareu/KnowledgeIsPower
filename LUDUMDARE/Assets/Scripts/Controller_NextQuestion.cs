c0063b34061abcee77ec9472d8a0f0f18ef342de
ludum <carbonelli.franscisco@gmail.com>
2018-04-21 17:31:39 -0300

    dd

---
+++ LUDUMDARE/Assets/Scripts/Controller_NextQuestion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_NextQuestion : MonoBehaviour {
+
+    private Question_Show questions;
+
+	// Use this for initialization
+	void Start () {
+        GameObject g = GameObject.Find("QuestionsHolder");
+
+        this.questions = g.GetComponent<Question_Show>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("n"))
+        {
+            this.questions.changing = true;
+        }
+    }
+}
