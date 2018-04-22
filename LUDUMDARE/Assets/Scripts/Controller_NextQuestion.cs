1d6d62026dd27c0021c61053f1eefa92fa76fea9
Francisco Carbonelli <carbonelli.francisco@gmail.com>
2018-04-22 19:55:01 -0300

    mostrar preguntas

--- LUDUMDARE/Assets/Scripts/Controller_NextQuestion.cs
+++ LUDUMDARE/Assets/Scripts/Controller_NextQuestion.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Controller_NextQuestion : MonoBehaviour {
 
-    public Question_Show questions;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +14,7 @@ public class Controller_NextQuestion : MonoBehaviour {
     void Update () {
         if (Input.GetKeyDown("n"))
         {
-            this.questions.changing = true;
+           
         }
     }
 }
---
+++ LUDUMDARE/Assets/Scripts/MostrarPreguntas.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MostrarPreguntas : MonoBehaviour {
+
+    public Text TxtPregunta, TxtPespuesta1, TxtRespuesta2, TxtPuntos;
+
+    public string[] preguntas, respuestas1, respuestas2;
+    int lvl = 0;
+    bool started, resp; //falso-izquierda true-derecha
+    int points = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (started)
+        {
+            TxtPregunta.text = preguntas[lvl].ToString();
+            TxtPespuesta1.text = respuestas1[lvl].ToString();
+            TxtRespuesta2.text = respuestas2[lvl].ToString();
+        }
+
+        TxtPuntos.text = points.ToString();
+    }
+    public void respCorrecta()
+    {
+        int ran = Random.Range(10, 15);
+        switch (lvl)
+        {
+            //NIVEL 1
+            case 0:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+
+            case 1:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+
+            case 2:
+                if (resp == true) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+
+            //NIVEL 2
+            case 3:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+            case 4:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+            case 5:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+
+            //NIVEL 3
+            case 6:
+                if (resp == true) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+            case 7:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+            case 8:
+                if (resp == true) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+
+            //NIVEL 4
+            case 9:
+                if (resp == false) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+            case 10:
+                if (resp == true) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+            case 11:
+                if (resp == true) //correcto
+                {
+                    points += 121 * ran; //* DIFERENCIA_DECOLOR 
+                }
+                else
+                {
+                    points += 40 * ran; // * (diferencia/2)
+                }
+                break;
+
+
+            //NIVEL 5
+            case 12:
+                points += ran * ran + ran;
+                break;
+            case 13:
+                points += ran * ran + ran;
+                break;
+            case 14:
+                points += ran * ran + ran;
+                break;
+            case 15:
+                points += ran * ran + ran;
+                break;
+            case 16:
+                points += ran * ran + ran;
+                break;
+
+        }
+    }
+}
+
